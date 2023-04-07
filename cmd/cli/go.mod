143e70ec72dbce514518975be7622e372c31a262
marat <gumyarov@bzaem.com>
2023-04-07 21:41:03 +0300

    feat(client): added menu

---
+++ cmd/cli/go.mod
@@ -0,0 +1,12 @@
+module github.com/brisk84/gophkeeper-cli
+
+go 1.19
+
+require github.com/spf13/cobra v1.7.0
+
+require (
+	github.com/buger/goterm v1.0.4 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20210331175145-43e1dd70ce54 // indirect
+)
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/user"
 	"time"
 
 	"github.com/brisk84/gophkeeper/api"
+	"github.com/manifoldco/promptui"
+	"github.com/nexidian/gocliselect"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
@@ -42,7 +46,123 @@ func intTests(ctx context.Context, c api.GophKeeperClient) int {
 	return 0
 }
 
+func menu() {
+	// tm.Clear() // Clear current screen
+	// // for {
+	// // 	// By moving cursor to top-left position we ensure that console output
+	// // 	// will be overwritten each time, instead of adding new.
+	// // 	tm.MoveCursor(1, 1)
+	// // 	tm.Println("Current Time:", time.Now().Format(time.RFC1123))
+	// // 	tm.Flush() // Call it every time at the end of rendering
+	// // 	time.Sleep(time.Second)
+	// // }
+	// tm.Println("GophKeeper client")
+	// tm.Println("Enter command")
+	// tm.Println("r - register")
+	// tm.Println("l - login")
+	// tm.Screen.Read()
+	// fmt.Println("GophKeeper client")
+	// fmt.Println("Enter command")
+	// fmt.Println("r - register")
+	// fmt.Println("l - login")
+	// var comm string
+	// fmt.Scan(&comm)
+	// fmt.Println(comm)
+	menu := gocliselect.NewMenu("Welcome to GophKeeper! Choose a command")
+
+	menu.AddItem("Register", "register")
+	menu.AddItem("Login", "login")
+	menu.AddItem("Exit", "exit")
+
+	choice := menu.Display()
+
+	fmt.Printf("Choice: %s\n", choice)
+}
+
+func Register() {
+	validate := func(input string) error {
+		if len(input) < 3 {
+			return errors.New("Username must have more than 3 characters")
+		}
+		return nil
+	}
+
+	var username string
+	u, err := user.Current()
+	if err == nil {
+		username = u.Username
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Username",
+		Validate: validate,
+		Default:  username,
+	}
+
+	login, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	// fmt.Printf("Your username is %q\n", result)
+
+	validate = func(input string) error {
+		if len(input) < 6 {
+			return errors.New("Password must have more than 6 characters")
+		}
+		return nil
+	}
+
+	prompt = promptui.Prompt{
+		Label:    "Password",
+		Validate: validate,
+		Mask:     '*',
+	}
+
+	password, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	// fmt.Printf("Your password is %q\n", result)
+
+	tlsCredentials, err := loadTLSCredentials()
+	if err != nil {
+		log.Fatal("cannot load TLS credentials: ", err)
+	}
+	cc1, err := grpc.Dial("0.0.0.0:4343", grpc.WithTransportCredentials(tlsCredentials))
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+
+	c := api.NewGophKeeperClient(cc1)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	// exitCode := intTests(ctx, c)
+	// fmt.Println("ExitCode:", exitCode)
+
+	resp, err := c.Register(ctx, &api.RegisterLoginReq{
+		Login:    login,
+		Password: password,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.Token)
+
+	cancel()
+	cc1.Close()
+	// os.Exit(exitCode)
+}
+
 func main() {
+	Register()
+	return
+
 	tlsCredentials, err := loadTLSCredentials()
 	if err != nil {
 		log.Fatal("cannot load TLS credentials: ", err)
